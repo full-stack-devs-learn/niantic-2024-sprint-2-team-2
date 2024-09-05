05db235863c0d68cbd69daf7aad8c0df2104af55
Chin Antalan <152952438+chinsomnia@users.noreply.github.com>
2024-09-05 14:33:10 -0700

    Created getAnswersByQuestionId()

--- src/main/java/com/nianti/services/AnswerDao.java
+++ src/main/java/com/nianti/services/AnswerDao.java
@@ -7,6 +7,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -22,6 +23,30 @@ public class AnswerDao
 
     public List<Answer> getAnswersByQuestionId(int questionId)
     {
-        return null;
+        ArrayList<Answer> answers = new ArrayList<>();
+
+        String sql = """
+                SELECT answer_id
+                    , question_id
+                    , answer_text
+                    , is_correct
+                FROM answer
+                WHERE question_id = ?;
+                """;
+
+        var row = jdbcTemplate.queryForRowSet(sql, questionId);
+
+        while(row.next())
+        {
+            int answerId = row.getInt("answer_id");
+            String answerText = row.getString("answer_text");
+            Boolean isCorrect = row.getBoolean("is_correct");
+
+            Answer answer = new Answer(answerId, questionId, answerText, isCorrect);
+
+            answers.add(answer);
+        }
+
+        return answers;
     }
 }
