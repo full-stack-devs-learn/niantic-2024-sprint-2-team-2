3ed91a53276287133fd36d72bceb9cc1fff292e6
Chin Antalan <152952438+chinsomnia@users.noreply.github.com>
2024-09-05 14:31:16 -0700

    Created questionsByQuizId()

--- src/main/java/com/nianti/controllers/QuestionController.java
+++ src/main/java/com/nianti/controllers/QuestionController.java
@@ -1,6 +1,25 @@
 package com.nianti.controllers;
 
+import com.nianti.services.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
 public class QuestionController
 {
+    @Autowired
+    private QuestionDao questionDao;
 
+    @GetMapping("/quiz/{quizId}/{questionId}")
+    public String questionsByQuizId(Model model, @PathVariable int quizId, @PathVariable int questionId)
+    {
+        var questions = questionDao.getQuestionsByQuizId(quizId);
+
+        model.addAttribute("questions", questions);
+        return "quiz/fragments/quiz-questions";
+    }
 }
