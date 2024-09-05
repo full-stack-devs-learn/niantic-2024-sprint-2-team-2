9eb67218ad607926a32eb32f7f3732b9121473e6
Eri (Mac) <eri-ishikawa@proton.me>
2024-09-05 15:42:31 -0700

    added fetch method in JS and properly displays one question

--- src/main/java/com/nianti/controllers/QuestionController.java
+++ src/main/java/com/nianti/controllers/QuestionController.java
@@ -1,8 +1,8 @@
 package com.nianti.controllers;
 
 import com.nianti.services.QuestionDao;
+import com.nianti.services.AnswerDao;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +14,18 @@ public class QuestionController
     @Autowired
     private QuestionDao questionDao;
 
+    @Autowired
+    private AnswerDao answerDao;
+
     @GetMapping("/quiz/{quizId}/{questionId}")
     public String questionsByQuizId(Model model, @PathVariable int quizId, @PathVariable int questionId)
     {
-        var questions = questionDao.getQuestionsByQuizId(quizId);
+        var question = questionDao.getQuestionById(questionId);
+        var answers = answerDao.getAnswersByQuestionId(questionId);
 
-        model.addAttribute("questions", questions);
+        model.addAttribute("question", question);
+        model.addAttribute("answers", answers);
+
         return "quiz/fragments/quiz-questions";
     }
 }
--- src/main/resources/static/js/quiz.js
+++ src/main/resources/static/js/quiz.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // on load
+    loadPage();
 });
+
+function loadPage()
+{
+    const container = document.getElementById("quiz-container");
+    const startButton = document.getElementById("start");
+    let quizId = 1;
+    let questionId = 1;
+
+    startButton.addEventListener("click", () => {
+        url = `/quiz/${quizId}/${questionId}`;
+
+        fetch(url).then(response => {
+        if(response.status === 200)
+        {
+            return response.text();
+        }
+        throw new Error(response);
+        }).then(data => {
+            container.innerHTML = data;
+        }).catch(error => {
+            console.log(error)
+        });
+    })
+}
