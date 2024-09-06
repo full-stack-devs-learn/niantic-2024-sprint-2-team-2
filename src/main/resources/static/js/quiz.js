6152f370ddb045b6bf91bbffcddbe98be0f1aa16
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-09-05 23:41:40 -0700

    Added getQuestionCount() function using Questions Api

--- src/main/resources/static/js/quiz.js
+++ src/main/resources/static/js/quiz.js
@@ -1,6 +1,7 @@
 let quizId = 1;
-let questionId = 1;
+let questionNumber = 1;
 let score = 0;
+let numberOfQuestions = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
     startQuiz();
@@ -19,7 +20,7 @@ function startQuiz()
 
 function loadQuestion()
 {
-    const url = `/quiz/${quizId}/${questionId}`;
+    const url = `/quiz/${quizId}/${questionNumber}`;
     const quizContainer = document.getElementById("quiz-container");
 
     fetch(url).then(response => {
@@ -30,6 +31,7 @@ function loadQuestion()
     throw new Error(response);
     }).then(data => {
         quizContainer.innerHTML = data;
+        getQuestionCount();
         evaluateAnswer();
     }).catch(error => {
         console.log(error)
@@ -44,16 +46,45 @@ function evaluateAnswer()
         event.preventDefault();
         const selection = event.target.answer.value;
 
-        if (selection) 
+        if (selection == 'true')
         {
             score++;
         }
 
-        questionId++;
+//        alert(`selection: ${selection}, score: ${score}`);
+
+        questionNumber++;
+        displayFinalScore();
         loadQuestion();
     })
 }
 
+function getQuestionCount()
+{
+    const url = `/api/quiz/${quizId}`;
+
+    fetch(url).then(response => response.text())
+              .then(data => { numberOfQuestions = +data; })
+}
+
+function displayFinalScore()
+{
+    // when we get to final question
+    // on the final question, replace NEXT with SUBMIT button
+//    let questionNumberminus
+    let q = questionNumber;
+    if (q > numberOfQuestions)
+    {
+        const nextButton = document.getElementById("next-btn");
+        nextButton.textContent = "Submit";
+    }
+    alert(`question number: ${questionNumber}, numberOfQuestions is: ${numberOfQuestions}, score is: ${score}, `);
+
+
+
+    // hide/replace the question and answer fragment
+    // then display final SCORE on a new fragment
+}
 
 // use fetch to get the Answer from server-side?
 //    const url = `quiz/answer/${selection}`;
