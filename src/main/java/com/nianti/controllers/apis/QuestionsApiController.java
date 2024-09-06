5dd1594ab0f2651ff8f255f1abbdc1874f213e8c
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-09-05 23:40:37 -0700

    Created getQuestionsCount Api controller

---
+++ src/main/java/com/nianti/controllers/apis/QuestionsApiController.java
@@ -0,0 +1,22 @@
+package com.nianti.controllers.apis;
+
+import com.nianti.services.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QuestionsApiController
+{
+    @Autowired
+    private QuestionDao questionDao;
+
+    @GetMapping("/api/quiz/{quizId}")
+    public int getQuestionsCount(@PathVariable int quizId)
+    {
+        int count = questionDao.getQuestionsCountByQuizId(quizId);
+
+        return count;
+    }
+}
