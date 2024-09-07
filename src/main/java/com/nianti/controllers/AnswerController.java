044d0c28988a506784e7db2a1c8da09b486c0a75
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-09-07 05:38:31 -0700

    Created getMapping to Add Answer page

--- src/main/java/com/nianti/controllers/AnswerController.java
+++ src/main/java/com/nianti/controllers/AnswerController.java
@@ -1,7 +1,9 @@
 package com.nianti.controllers;
 
 import com.nianti.models.Answer;
+import com.nianti.models.Question;
 import com.nianti.services.AnswerDao;
+import com.nianti.services.QuestionDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class AnswerController
 {
-//    @Autowired
-//    private AnswerDao answerDao;
+    @Autowired
+    private QuestionDao questionDao;
+
+    @Autowired
+    private AnswerDao answerDao;
 //
 //    @GetMapping("/quizzes/{quizId}/{questionId}")
 //    public String answersByQuestionId(Model model, @PathVariable int quizId, @PathVariable int questionId)
@@ -22,4 +27,18 @@ public class AnswerController
 //        model.addAttribute("answers", answers);
 //        return "quiz/fragments/answers-list";
 //    }
+
+    @GetMapping("/question/{questionId}/answer/add")
+    public String addAnswer(Model model, @PathVariable int questionId)
+    {
+        var questions = questionDao.getAllQuestionsByQuizId(questionId);
+        var questionText = questionDao.getQuestionText(questionId);
+
+        model.addAttribute("answer", new Answer());
+        model.addAttribute("questions", questions);
+        model.addAttribute("questionText", questionText);
+        model.addAttribute("action", "add");
+
+        return "answer/add-edit";
+    }
 }
