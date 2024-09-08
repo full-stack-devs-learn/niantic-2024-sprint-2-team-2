928d490ef84486abdd4ab4e61f83eb5a2ec4bcfc
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-09-08 03:05:51 -0700

    Updated add-answer js script to setup validation for only 1 correct answer
    Work in progress. Not yet working

--- src/main/resources/static/js/answer-add.js
+++ src/main/resources/static/js/answer-add.js
@@ -1,16 +1,64 @@
+let questionId = 1;
+let hasCorrect = false;
+
 document.addEventListener("DOMContentLoaded", () => {
-    const addAnswer = document.getElementById("add-answer");
-    const answerText = document.getElementById("answer-text");
+    getQuestionId();
 
-    answerText.addEventListener("input", () => {
-        addAnswer.classList.remove("was-validated");
-    })
-
-    addAnswer.addEventListener("submit", (event) => {
-        if(!addAnswer.checkValidity())
-        {
-            event.preventDefault();
-            addAnswer.classList.add("was-validated");
-        }
-    })
 })
+
+function getQuestionId()
+{
+    questionId = document.getElementById("question-id").value;
+    alert(questionId);
+
+    checkForCorrectAnswer();
+}
+
+function checkForCorrectAnswer()
+{
+    // api returns a boolean
+    const url = `/api/question/${questionId}`;
+
+    fetch(url).then(response => response.text())
+    .then(data => {
+        if(data == "true")
+        {
+            hasCorrect = true;
+        }})
+    .then(allowAddCorrect())
+    .catch(error => {
+            console.error("Error fetching data:", error);
+        });;
+}
+
+function allowAddCorrect()
+{
+    const checkbox = document.getElementById("correct-checkbox");
+    const submitBtn = document.getElementById("submit-btn");
+//    const invalidCorrect = document.getElementById("invalid-correct");
+    const form = document.getElementById("add-answer");
+
+    // display error if there is already a correct answer
+    if(hasCorrect) // should not allow user to add answer as correct
+    {
+        event.preventDefault();
+        form.addEventListener("input", () => {
+            submitBtn.classList.add("was-validated");
+        });
+    }
+    else
+    {
+        // allow add answer
+        checkbox.addEventListener("input", () => {
+            checkbox.classList.remove("was-validated");
+        })
+
+        submitBtn.addEventListener("submit", (event) => {
+            if(!form.checkValidity())
+            {
+                event.preventDefault();
+                submitBtn.classList.add("was-validated");
+            }
+        });
+    }
+}
