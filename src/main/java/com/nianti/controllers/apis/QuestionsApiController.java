a20ef80432ccb10939a705cf0f972f5ea66a8555
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-09-08 03:03:03 -0700

    Created api controller to check if there is an existing correct answer

--- src/main/java/com/nianti/controllers/apis/QuestionsApiController.java
+++ src/main/java/com/nianti/controllers/apis/QuestionsApiController.java
@@ -1,5 +1,7 @@
 package com.nianti.controllers.apis;
 
+import com.nianti.models.Answer;
+import com.nianti.services.AnswerDao;
 import com.nianti.services.QuestionDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +14,33 @@ public class QuestionsApiController
     @Autowired
     private QuestionDao questionDao;
 
+    @Autowired
+    private AnswerDao answerDao;
+
     @GetMapping("/api/quizzes/{quizId}")
     public int getQuestionsCount(@PathVariable int quizId)
     {
         int count = questionDao.getQuestionsCountByQuizId(quizId);
 
         return count;
+    }
+
+    // Check if answer already exists for the question
+    @GetMapping("/api/question/{questionId}")
+    public Boolean hasCorrectAnswer(@PathVariable int questionId)
+    {
+        // Get answers of question id
+        var answers = answerDao.getAnswersByQuestionId(questionId);
+
+        Boolean correctAnswer = false;
+
+        for(Answer answer: answers)
+        {
+            if(answer.isCorrect())
+            {
+                correctAnswer = true;
+            }
+        }
+        return correctAnswer;
     }
 }
