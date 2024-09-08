db2699f326334c62e36dc110e48d9da2bd732b37
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-09-07 19:15:03 -0700

    Added client-side validation for answer add/edit

--- src/main/resources/static/js/question-details.js
+++ src/main/resources/static/js/question-details.js
@@ -5,20 +5,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const cancelBtn = document.getElementById("cancel-btn");
 
     deleteBtn.addEventListener("click", () => {
-//        answerInfo.forEach((element) => element.classList.add("hide"));
-
-        // Iterate through the retrieved elements and add the necessary class names.
-          for(let i = 0; i < answerInfo.length; i++)
+        for(let i = 0; i < answerInfo.length; i++)
           {
               answerInfo[i].classList.add("hide");
           }
-//        answerInfo.classList.add("hide");
-//        array1.forEach((element) => console.log(element));
         deleteCancelBtn.classList.remove("hide");
     })
 
     cancelBtn.addEventListener("click", () => {
-//        editDeleteBtn.classList.remove("hide");
         for(let i = 0; i < answerInfo.length; i++)
           {
               answerInfo[i].classList.remove("hide");
